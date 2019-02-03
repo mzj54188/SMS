6ad6554d336aa5ffe22b930a8610f38be0587bfd
BboyXing <1570967263@qq.com>
2019-02-03 21:25:49 +0800

    2.3

---
+++ demo/src/components/Nav/Nav.vue
@@ -0,0 +1,51 @@
+<template>
+     <el-menu 
+        background-color="#2d3a4b" 
+        unique-opened
+        active-text-color="rgb(64, 158, 255)"
+        :default-active="this.$route.path"
+        router
+        >
+        <!-- 导航一 -->
+        <el-submenu index="1">
+            <template slot="title"><i class="el-icon-setting"></i>系统管理</template>
+                <el-menu-item index="/">系统信息</el-menu-item>
+        </el-submenu>
+        <!-- 导航二 -->
+        <el-submenu index="2">
+            <template slot="title"><i class="el-icon-menu"></i>账号管理</template>
+                <el-menu-item index="/accountmanage">账号管理</el-menu-item>
+                <el-menu-item index="/accountadd">添加账号</el-menu-item>
+                <el-menu-item index="/passwordmodify">密码修改</el-menu-item>
+        </el-submenu>
+        <!-- 导航三 -->
+        <el-submenu index="3">
+            <template slot="title"><i class="el-icon-goods"></i>商品管理</template>
+                <el-menu-item index="/goodsmanage">商品管理</el-menu-item>
+                <el-menu-item index="/goodsadd">添加商品</el-menu-item>
+        </el-submenu>
+        <!-- 导航四 -->
+        <el-submenu index="4">
+            <template slot="title"><i class="el-icon-date"></i>统计管理</template>
+                <el-menu-item index="/salestatistics">销售统计</el-menu-item>
+        </el-submenu>
+    </el-menu>
+</template>
+<script>
+export default {};
+</script>
+<style lang="less">
+.el-menu {
+  border: none;
+  .el-submenu {
+    .el-submenu__title {
+      color: #fff;
+    }
+  }
+  .el-menu-item {
+    color: #fff;
+  }
+}
+</style>
+
+
--- demo/src/views/Index/Index.vue
+++ demo/src/views/Index/Index.vue
@@ -1,13 +1,76 @@
 <template>
-    <div class="index">
-        首页
-    </div>
+    <!-- <div class="index"> -->
+        <!-- 内容容器 -->
+        <el-container>
+            <!-- 左侧 -->
+            <el-aside width="200px">
+                <!-- 自定义导航组件 -->
+               <Nav></Nav>
+            </el-aside>
+
+            <!-- 右侧 -->
+            <el-container>
+                <!-- 右侧头部 -->
+                <el-header>
+                    <!-- 头部组件 -->
+                    <Top></Top>
+                </el-header>
+                <!-- 右侧中间 -->
+                <el-main>
+                    <!-- 路由出口 -->
+                    <router-view />
+                </el-main>
+                <!-- 右侧尾部 -->
+                <el-footer>
+                    <!-- 尾部组件 -->
+                    <Bottom></Bottom>
+                </el-footer>
+            </el-container>
+        </el-container>
+    <!-- </div> -->
 </template>
 <script>
+// 引入导航组件
+import Nav from '@/components/Nav/Nav.vue';
+// 引入头部组件
+import Top from '@/components/Top/Top.vue';
+// 引入尾部组件
+import Bottom from '@/components/Bottom/Bottom.vue';
+
 export default {
-    
-}
+    components: {  // 注册组件
+        Nav,
+        Top,
+        Bottom
+    }
+};
 </script>
 <style lang="less">
-
+.el-container {
+  height: 100%;
+  // 左侧
+  .el-aside {
+    background-color: #2d3a4b;
+    text-align: center;
+  }
+  // 右侧中间
+  .el-main {
+    background-color: #fff;
+    color: #333;
+    text-align: center;
+  }
+  // 右侧头和尾
+  .el-header,
+  .el-footer {
+    background-color: #fff;
+    color: #2d3a4b;
+    text-align: center;
+    line-height: 60px;
+  }
+   .el-header {
+       border-bottom: 1px solid #ccc;
+   }
+}
 </style>
+
+
